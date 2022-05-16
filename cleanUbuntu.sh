808591c53cc27e5ad864f5d2d5107aa50c590458
Vasanth V V <vasanthvdev@gmail.com>
2022-05-17 00:58:46 +0530

    added snap firefox

--- cleanUbuntu.sh
+++ cleanUbuntu.sh
@@ -1,2 +1,3 @@
 sudo apt remove aisleriot gnome-calendar gnome-mahjongg gnome-mines rhythmbox gnome-robots shotwell gnome-sudoku gnome-games totem -y
+sudo snap remove firefox
 sudo apt autoremove -y
